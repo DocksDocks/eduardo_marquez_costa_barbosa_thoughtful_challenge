d634237f98204e95db1c139e0579ac1bec251521
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2024-08-30 22:03:57 -0300

    [Refactoring] - Extracting data working
    removing comments in [tasks.py]

--- tasks.py
+++ tasks.py
@@ -4,12 +4,12 @@ def the_process():
     browser = ExtendedSelenium()
     try:
         browser.open_site("https://apnews.com/")
-        browser.close_popup_if_present()  # Close the popup if it appears
-        browser.click_search_button()  # Click the search button after closing the popup
-        browser.type_and_submit_search_query("COVID")  # Type "COVID" into the search input
-        browser.click_and_select_category()  # Click and select the "Stories" category
-        browser.select_sort_by_newest()  # Sort by Newest
-        browser.extract_news_data_and_store()  # Extract news data and store in Excel
+        browser.close_popup_if_present()
+        browser.click_search_button()
+        browser.type_and_submit_search_query("COVID")
+        browser.click_and_select_category()
+        browser.select_sort_by_newest()
+        browser.extract_news_data_and_store()
     finally:
         browser.quit_driver()
 
